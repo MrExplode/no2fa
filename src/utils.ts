5bca974c423b56211dea54136cf99c489be0798f
MrExplode <me@sunstorm.rocks>
2024-02-20 14:54:45 +0100

    docs: add util documentation

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,17 @@
 import $ from 'jquery'
 
+/**
+ * @returns true when the totp token modal is visible
+ */
 export function isTokenModelOpen(): boolean {
     return $('#tokenValidationModal').css('display') !== 'none'
 }
 
+/**
+ * Writes the totp token into the input field and clicks the submit button.
+ *
+ * @param token the totp result
+ */
 export function writeToken(token: string) {
     const input = $('input[name="token"]')
     input.val(token)
