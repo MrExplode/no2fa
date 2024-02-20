d5a3a92b52f2923f692054b3a53f1fa9f3b8ef44
MrExplode <me@sunstorm.rocks>
2024-02-20 17:21:55 +0100

    feat: settings management

--- src/handlers/settings.ts
+++ src/handlers/settings.ts
@@ -1,11 +1,32 @@
+import $ from 'jquery'
 import { Handler } from './handler'
-import { is2faPage } from '../utils'
+import { generateButtons, is2faPage } from '../utils'
+import { observe } from '../observer'
 
 export class SettingsHandler extends Handler {
+    private unsubscribe: (() => void) | null = null
+    private injected: boolean = false
+
     shouldActivate(): boolean {
         return is2faPage()
     }
+
     load(): void {
-        console.log('loading settings')
+        this.unsubscribe = observe('#form1', this.onMutations)
+    }
+
+    unload(): void {
+        if (this.unsubscribe != null) this.unsubscribe()
+    }
+
+    private onMutations() {
+        if (!this.injected) {
+            const button = $(
+                'input[name="upFunction$c_options$UpdatePanel1$tab$ctl02$ctl01$upmodalTwoFactorSetup$ctl02$twoFactorSetup$ctl00$Setup"]'
+            )
+            if (button.length == 0) return
+            generateButtons(button.parent())
+            this.injected = true
+        }
     }
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
 import $ from 'jquery'
+import { GM_deleteValue, GM_getValue, GM_setValue } from '$'
 
 /**
  * @returns true when the totp token modal is visible
@@ -24,4 +25,32 @@ export function writeToken(token: string) {
 export function is2faPage(): boolean {
     const tab = $('#c_options_ctl02_tab')
     return tab.length != 0 && tab.hasClass('ajax__tab_active')
+}
+
+/**
+ * Generates the management buttons.
+ *
+ * @param parent the parent container to insert buttons
+ */
+export function generateButtons(parent: JQuery<HTMLElement>) {
+    const enabled = GM_getValue('totpSecret', null) != null
+    parent.append(
+        `<input id="no2faButton" type="button" class="button" value="${enabled ? 'Update Bypass' : 'Enable Bypass'}"/>`
+    )
+    $('#no2faButton').on('click', (e) => {
+        e.preventDefault()
+        GM_setValue('totpSecret', $('#twoFactorSetup_Secret').text())
+        alert(enabled ? 'Bypass updated!' : 'Bypass enabled!')
+    })
+    // delete options
+    if (enabled) {
+        parent.append(
+            `<input id="no2faDelete" type="button" class="button" value="Disable Bypass"/>`
+        )
+    }
+    $('#no2faDelete').on('click', (e) => {
+        e.preventDefault()
+        GM_deleteValue('totpSecret')
+        alert('Bypass disabled!')
+    })
 }
