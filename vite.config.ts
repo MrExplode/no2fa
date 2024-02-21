a113a553eff2cee67b51e69a8a07a1049364f3e6
MrExplode <me@sunstorm.rocks>
2024-02-21 08:39:08 +0100

    fix: disable minify

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import monkey from 'vite-plugin-monkey'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    build: {
-        minify: 'esbuild'
-    },
     plugins: [
         monkey({
             entry: 'src/main.ts',
